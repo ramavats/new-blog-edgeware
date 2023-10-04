e76ab8951d76e21d9714c6e87b6023776b710a08
Rama Shankar Jha <guy55222@gmail.com>
2023-10-04 13:48:06 +0530

    modified layout

--- app/page.tsx
+++ app/page.tsx
@@ -37,24 +37,22 @@ export default async function IndexPage() {
       {data.map((post) => (
         <li key={post._id} className="py-4">
           <article className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
-            <div>
-              <p className="text-base font-medium leading-6 text-teal-500">
-                {new Date(post._createdAt).toISOString().split('T')[0]}
-              </p>
-            </div>
             <Link href={`/post/${post.slugUrl}`} prefetch className="space-y-3 xl:col-span-3">
-              <div>
+              <div className="flex justify-between">
                 <h3 className="text-2xl font-bold leading-8 tracking-tight text-gray-900 dark:text-gray-100">
                   {post.title}
                 </h3>
+                <p className="text-base font-medium leading-6 text-teal-500">
+                {new Date(post._createdAt).toISOString().split('T')[0]}
+              </p>
               </div>
-
+              <div className="h-96 w-full relative">
+              <Image fill src={post.imageUrl} alt="cover image" className="h-full w-full object-cover rounded-lg" />
+              </div>
               <p className="pros max-w-none text-gray-500 dark:text-gray-400 line-clamp-3">
                 {post.excerpt}
               </p>
-              <div className="h-56 w-full relative">
-              <Image fill src={post.imageUrl} alt="cover image" className="h-full w-full object-cover" />
-              </div>
+              
               
             </Link>
           </article>
